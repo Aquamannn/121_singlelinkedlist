81c8f3037e50592fc63698e9b96d103db192f09c
Lenovo <9.3razzinayyuri@gmail.com>
2024-04-26 10:34:30 +0700

    add void addnode

--- 121_singlelinkedlist.cpp
+++ 121_singlelinkedlist.cpp
@@ -6,3 +6,30 @@ struct Node {
 	string name;
 	Node* next;
 };
+
+Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukan NIM: ";
+	cin >> nim;
+	cout << "Masukan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "Nim sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+
+}
